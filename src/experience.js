e3a237424e5e2e56df0d4146922221486d91da23
Jad Mintun <jadmintun@gmail.com>
2021-08-24 17:46:00 -0400

    model 48

--- src/experience.js
+++ src/experience.js
@@ -8,11 +8,9 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader"
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
 import { constants, setAssetsLoaded, setThreeScene, getModel, setModel, setOrbitControls, getCamera, setCamera } from "./model"
-
 import { createOrbitPositionTestSphere } from "./common/test-sphere"
 
-const fov = 30,
-  showOrbitTestSphere = false,
+const showOrbitTestSphere = false,
   directional1Position = [5, 30, 3],
   // envMapExposure = 0.5,
   // directionali1Intensity = 0.5,
@@ -21,28 +19,20 @@ const fov = 30,
   directionali1Intensity = 1,
   ambientIntensity = 1,
   canvasContainer = document.querySelector(".canvas-container"),
-  modelSrc = "model/model-45.glb"
+  modelSrc = "model/model-48.glb"
 
 let canvas = null,
+  fov = null,
   scene = null,
   renderer = null,
   camera = null,
   controls = null,
-  mixer = null,
   pmremGenerator = null,
   sizes = null,
-  delta = 0,
-  clock = new THREE.Clock(),
   camPos = [-40, 25, 60],
   controlsPos = [5, 8, -5]
 
 let sun = null,
-  earth = null,
-  jupiter = null,
-  mars = null,
-  mercury = null,
-  saturn = null,
-  venus = null,
   planets01 = null,
   planets02 = null,
   planets03 = null,
@@ -103,6 +93,7 @@ const init = () => {
   renderer.toneMapping = THREE.ACESFilmicToneMapping
   renderer.toneMappingExposure = envMapExposure
 
+  fov = window.innerWidth < 600 ? 45 : 30
   camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1, 3000)
   camera.position.set(camPos[0], camPos[1], camPos[2])
   scene.add(camera)
@@ -110,12 +101,9 @@ const init = () => {
 
   controls = new OrbitControls(camera, canvas)
   controls.enableDamping = true
+  controls.enableZoom = false
   controls.target.set(controlsPos[0], controlsPos[1], controlsPos[2])
   setOrbitControls(controls)
-
-  // controls.addEventListener("change", (e) => {
-  //   EventBus.publish("ORBIT_CHANGED", camera.position)
-  // })
 
   pmremGenerator = new THREE.PMREMGenerator(renderer)
   pmremGenerator.compileEquirectangularShader()
@@ -162,9 +150,9 @@ const loadModel = () => {
   }
   particlesGeometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
   const particlesMaterial = new THREE.PointsMaterial()
-  particlesMaterial.size = 0.7
+  particlesMaterial.size = 0.8
   particlesMaterial.sizeAttenuation = true
-  particlesMaterial.color = new THREE.Color("#ffffff")
+  // particlesMaterial.color = new THREE.Color("#ffffff")
 
   particlesMaterial.transparent = true
   particlesMaterial.alphaMap = particleTexture
@@ -204,16 +192,22 @@ const loadModel = () => {
      * Animation
      */
 
-    // if (gltf.animations.length > 0) {
-    //   mixer = new THREE.AnimationMixer(gltf.scene)
-    //   scene.add(gltf.scene)
-    //   gltf.animations.forEach((clip) => {
-    //     mixer.clipAction(clip).play()
-    //   })
-    // }
-
     gsap.from(getCamera().position, { duration: 2, z: 1000, delay: 0, ease: "power4.out" })
-    gsap.fromTo(getModel().rotation, { y: -0.3 }, { duration: 3, y: 0.4 })
+    gsap.fromTo(
+      getModel().rotation,
+      { y: -0.3 },
+      {
+        duration: 3.2,
+        y: 0.4,
+        ease: "power4.out",
+        onComplete: () => {
+          controls.minAzimuthAngle = -1.25
+          controls.maxAzimuthAngle = 0.4
+          controls.minPolarAngle = 1
+          controls.maxPolarAngle = 1.6
+        },
+      }
+    )
     gsap.fromTo(fanMotor.rotation, { y: 1.7 }, { duration: 6, y: 0, yoyo: true, repeat: -1, ease: "none" })
   })
 }
@@ -235,8 +229,11 @@ const tick = () => {
   sun.rotation.y -= 0.003
   blades.rotation.y += 0.15
 
-  // delta = clock.getDelta()
-  // if (mixer) mixer.update(delta)
+  // const distCameraToControlsTarget = camera.position.distanceTo(controls.target)
+  // if (distCameraToControlsTarget < 50) console.log("too close")
+  // if (distCameraToControlsTarget > 100) console.log("too far")
+
+  // console.log(controls.getAzimuthalAngle())
 }
 
 /**
