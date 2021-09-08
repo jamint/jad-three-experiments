dffad64d27b1d12833d3d7e211ab8b2c3564e074
Jad Mintun <jadmintun@gmail.com>
2021-09-08 09:30:32 -0400

    re-enable experience-01

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import EventBus from "eventing-bus"
 import { constants } from "./model"
 
 import "./styles/style.scss"
-// import "./experience-01"
-import "./experience-02"
+import "./experience-01"
+// import "./experience-02"
 import "./views/loading-view"
 
 /**
